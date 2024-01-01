12d14c86eb80a9871e094dea952831c1d45b5fae
Ian Bailey <IanKulin@kulin.com.au>
2024-01-01 10:26:28 +0800

    + Navbar bones

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -9,8 +10,8 @@ function App() {
           lg: `"nav nav" "aside main"`,
         }}
       >
-        <GridItem gridArea="nav" bg="tomato">
-          Nav
+        <GridItem gridArea="nav">
+          <NavBar />
         </GridItem>
         <Show above="lg">
           <GridItem gridArea="aside" bg="papayawhip">
