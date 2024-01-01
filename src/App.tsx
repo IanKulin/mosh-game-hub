ac30223212d4829051533521a752a7f3e723be4b
Ian Bailey <IanKulin@kulin.com.au>
2024-01-01 10:12:05 +0800

    + grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
-
-import { Button } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-
   return (
     <>
-    <Button colorScheme="blue">Button</Button>
-    <Button colorScheme="teal">Button</Button>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem gridArea="nav" bg="tomato">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem gridArea="aside" bg="papayawhip">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem gridArea="main" bg="gold">
+          Main
+        </GridItem>
+      </Grid>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
