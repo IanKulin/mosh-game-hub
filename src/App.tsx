0e1a3cf03acf8e43bdcc18f890d2928e9eede98a
Ian Bailey <IanKulin@kulin.com.au>
2024-01-01 08:58:23 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import { Button } from '@chakra-ui/react'
+
+function App() {
+
+  return (
+    <>
+    <Button colorScheme="blue">Button</Button>
+    <Button colorScheme="teal">Button</Button>
+    </>
+  )
+}
+
+export default App
