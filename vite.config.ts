6a0a6d9be1bc148f7dcc6258848f845bd1841c9f
Ian Bailey <IanKulin@kulin.com.au>
2024-01-01 09:02:14 +0800

    build steps for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/mosh-game-hub/",
   plugins: [react()],
+  build: {
+    outDir: 'docs', 
+  }
 })
