d1dd034d06aedf1a34d98a84e6b130b7045d19fe
Ian Bailey <IanKulin@kulin.com.au>
2024-01-02 10:26:40 +0800

    api fetch from iankulin.com

--- src/services/apiclient.ts
+++ src/services/apiclient.ts
@@ -1,8 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: "Some API Key that shouldn't be here",
-    }
+    baseURL: 'http://iankulin.com:3000'
 })
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/mosh-game-hub/",
+  //base: "/mosh-game-hub/",
   plugins: [react()],
   build: {
     outDir: 'docs', 
