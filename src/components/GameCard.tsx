50a3b04c568172c0185394a8b94cedb078dd514f
Ian Bailey <IanKulin@kulin.com.au>
2024-01-08 16:13:58 +0800

    cosmetic - game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -9,20 +9,18 @@ interface GameCardProps {
   game: Game;
 }
 
-
-
 const GameCard = ({ game }: GameCardProps) => {
   return (
     <Card>
       <Image src={getCroppedImageUrl(game.background_image)} alt={game.name} />
       <CardBody>
-        <TwoLineHeading fontSize={"2xl"}>{game.name}</TwoLineHeading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent='space-between' marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <TwoLineHeading fontSize={'2xl'}>{game.name}</TwoLineHeading>
       </CardBody>
     </Card>
   );
