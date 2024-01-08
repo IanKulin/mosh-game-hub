584d09997bce6903e864d8f6bff0d4c590c821c3
Ian Bailey <IanKulin@kulin.com.au>
2024-01-08 10:10:34 +0800

    make deps array non-optional to remove ternary in dependency array to make eslint happy

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -7,7 +7,7 @@ interface FetchResponse<T> {
   results: T[];
 }
 
-const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
+const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps: unknown[] = []) => {
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
@@ -28,7 +28,7 @@ const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?:
         setIsLoading(false);
       });
     return () => controller.abort();
-  }, deps ? [endpoint, ...deps]: [endpoint]);
+  }, [endpoint, ...deps]);
 
   return { data, error, isLoading };
 };
