b2b66547efce31c26880188fedb5b53de372698c
Ian Bailey <IanKulin@kulin.com.au>
2024-01-08 11:41:43 +0800

    update placeholder for games with missing images

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,6 @@
 
 export const getCroppedImageUrl = (url: string, width: number = 600, height: number = 400) => {
     // insert /crop/<width>/<height> after /media/ in the image URL
-    // if the image URL is null, return a placeholder image
     if (!url) {
         // if the image URL is null, return a placeholder image from the public folder
         return "/placeholder-game.jpg";
