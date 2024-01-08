a0b4f7010c34749fa63c72f7f446168d8033870f
Ian Bailey <IanKulin@kulin.com.au>
2024-01-08 19:56:34 +0800

    apiclient to https

--- src/services/apiclient.ts
+++ src/services/apiclient.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'http://api.iankulin.com'
-    //baseURL: 'http://localhost:3000'
+    baseURL: 'https://api.iankulin.com'
 })
