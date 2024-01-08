dc5d84c5d207e863eee78ea463ab05356165608e
Ian Bailey <IanKulin@kulin.com.au>
2024-01-08 11:27:04 +0800

    Generic image if no game image available

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,11 @@
 
 export const getCroppedImageUrl = (url: string, width: number = 600, height: number = 400) => {
     // insert /crop/<width>/<height> after /media/ in the image URL
+    // if the image URL is null, return a placeholder image
+    if (!url) {
+        // if the image URL is null, return a placeholder image from the public folder
+        return "/placeholder-game.jpg";
+    }
     const target = "media/";
     const insertIndex = url.indexOf(target) + target.length;
     const crop = `crop/${width}/${height}/`;
