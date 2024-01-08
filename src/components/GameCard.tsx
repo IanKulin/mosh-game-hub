5d0f3ffb16ec155cea2068ee91dc16c7bd0f7fcc
Ian Bailey <IanKulin@kulin.com.au>
2024-01-08 20:28:47 +0800

    Link to rawg to comply with API licence

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,11 @@
-import { Card, CardBody, HStack, Image } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  HStack,
+  Image,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
@@ -12,20 +19,32 @@ interface GameCardProps {
 
 const GameCard = ({ game }: GameCardProps) => {
   return (
-    <Card>
-      <Image src={getCroppedImageUrl(game.background_image)} alt={game.name} />
-      <CardBody>
-        <HStack justifyContent='space-between' marginBottom={3}>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-        <TwoLineHeading fontSize={'2xl'}>{game.name}</TwoLineHeading>
-        <Emoji rating={game.rating_top} />
-        
-      </CardBody>
-    </Card>
+    <LinkBox as="article">
+      <Card>
+        <Image
+          src={getCroppedImageUrl(game.background_image)}
+          alt={game.name}
+        />
+        <CardBody>
+          <HStack justifyContent="space-between" marginBottom={3}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+          <TwoLineHeading fontSize={"2xl"}>
+            <LinkOverlay
+              href={`https://rawg.io/games/${game.slug}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {game.name}
+            </LinkOverlay>
+          </TwoLineHeading>
+          <Emoji rating={game.rating_top} />
+        </CardBody>
+      </Card>
+    </LinkBox>
   );
 };
 
